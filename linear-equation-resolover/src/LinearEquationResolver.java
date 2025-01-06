8f1989221452b9ce8454867afdd4b2e3087df19f
An2806 <taman1842@gmail.com>
2025-01-06 11:12:56 +0700

    gptbn

--- linear-equation-resolover/src/LinearEquationResolver.java
+++ linear-equation-resolover/src/LinearEquationResolver.java
@@ -7,22 +7,19 @@ public class LinearEquationResolver {
         Scanner scanner = new Scanner(System.in);
         System.out.print("a: ");
         double a = scanner.nextDouble();
-
         System.out.print("b: ");
         double b = scanner.nextDouble();
-
         System.out.print("c: ");
         double c = scanner.nextDouble();
-
-        if (a != 0) {
-            double answer = (c -b) / a;
+        if (a != (double)0.0F) {
+            double answer = (c - b) / a;
             System.out.printf("Equation pass with x = %f!\n", answer);
+        } else if (b == c) {
+            System.out.print("The solution is all x!");
         } else {
-            if ( b == c) {
-                System.out.print("The solution is all x!");
-            } else {
-                System.out.print("No solution!");
-            }
+            System.out.print("No solution!");
         }
+
     }
 }
+
--- linear-equation-resolover/src/Main.java
+++ linear-equation-resolover/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +12,33 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+        }
+    }
+
+    public static class LinearEquationResolver {
+        public static void main(String[] args) {
+            System.out.println("Linear Equation Resolver");
+            System.out.println("Given a equation as 'a * x + b = c ', please enter constants:");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("a: ");
+            double a = scanner.nextDouble();
+
+            System.out.print("b: ");
+            double b = scanner.nextDouble();
+
+            System.out.print("c: ");
+            double c = scanner.nextDouble();
+
+            if (a != 0) {
+                double answer = (c -b) / a;
+                System.out.printf("Equation pass with x = %f!\n", answer);
+            } else {
+                if ( b == c) {
+                    System.out.print("The solution is all x!");
+                } else {
+                    System.out.print("No solution!");
+                }
+            }
         }
     }
 }
