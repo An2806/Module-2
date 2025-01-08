a4e302eaeff8bba2942bbc1668727dc4fa0c88e5
An2806 <taman1842@gmail.com>
2025-01-08 14:41:47 +0700

    BT bai2

--- remove-element-form-array/src/RemoveElementFormArray.java
+++ remove-element-form-array/src/RemoveElementFormArray.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
 
-public class RemoveELementFormArray {
+public class RemoveElementFormArray {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int [] array = {10, 4, 6, 7, 8, 6, 0, 0,0};
+        Scanner scanner = new Scanner(System.in);
+        //Bước 1: Khai báo và khởi tạo mảng số nguyên N phần tử cho trước.
+        int[] array = {10, 4, 6, 7, 8, 6, 0, 0, 0};
         int n = array.length;
 
+
+        //Hiển thị mảng ban đầu .
         System.out.println("Mảng ban đầu:");
         for (int i = 0; i < n; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
-
+        
+        //Yêu cầu người dùng nhập phần tử cần xóa
         System.out.print("Nhập phần tử cần xóa (X): ");
-        int x = sc.nextInt();
+        int x = scanner.nextInt();
 
-        int index_del = -1;
+        int indexDelete = -1;
         for (int i = 0; i < n; i++) {
             if (array[i] == x) {
-                index_del = i;
+                indexDelete = i;
                 break;
             }
         }
-        if (index_del == -1) {
-            System.out.println("Phần tử " + x+ "không tồn tại trong mảng.");
+        if (indexDelete == -1) {
+            System.out.println("Phần tử " + x + "không tồn tại trong mảng.");
         } else {
-            for (int i = index_del; i < array.length-1; i++) {
-                array[i] = array[i+1];
+            for (int i = indexDelete; i < array.length - 1; i++) {
+                array[i] = array[i + 1];
             }
-            array[array.length-1] = 0;
+            array[array.length - 1] = 0;
 
             System.out.println("Mảng sau khi xóa phần tử " + x + ":");
             for (int i = 0; i < n; i++) {
