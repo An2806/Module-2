f735a96ed35be20acaa36153b108a2265e867a62
An2806 <taman1842@gmail.com>
2025-01-13 15:45:41 +0700

    bai6 TH

--- mini-test/src/ProgrammingBook.java
+++ mini-test/src/ProgrammingBook.java
@@ -1,8 +1,8 @@
-public class ProgrmmingBook extends Book {
+public class ProgrammingBook extends Book {
     private String language;
     private String framework;
 
-    public ProgrmmingBook (int bookCode, String name, double price, String author,String language, String framework) {
+    public ProgrammingBook(int bookCode, String name, double price, String author, String language, String framework) {
         super (bookCode, name, price, author);
         this.language = language;
         this.framework = framework;
@@ -22,5 +22,10 @@ public class ProgrmmingBook extends Book {
 
     public void setFramework(String framework) {
         this.framework = framework;
+    }
+
+    @Override
+    public double getPriceAfterDiscount() {
+        return getPrice() * 0.95; // 5% discount
     }
 }
