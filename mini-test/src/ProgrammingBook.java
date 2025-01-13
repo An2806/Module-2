2722198bfde66159e490c572a5a805f5da8877c8
An2806 <taman1842@gmail.com>
2025-01-13 15:05:43 +0700

    mini test

---
+++ mini-test/src/ProgrammingBook.java
@@ -0,0 +1,26 @@
+public class ProgrmmingBook extends Book {
+    private String language;
+    private String framework;
+
+    public ProgrmmingBook (int bookCode, String name, double price, String author,String language, String framework) {
+        super (bookCode, name, price, author);
+        this.language = language;
+        this.framework = framework;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+}
