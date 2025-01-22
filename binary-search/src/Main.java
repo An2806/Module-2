d9ed8197dc00374aa36520de144d9b51e55ea22e
An2806 <taman1842@gmail.com>
2025-01-22 09:30:06 +0700

    first commit

---
+++ binary-search/src/Main.java
@@ -0,0 +1,13 @@
+//import static java.util.Collections.binarySearch;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println(binarySearch(list, 2));
+//        System.out.println(binarySearch(list, 11));
+//        System.out.println(binarySearch(list, 79));
+//        System.out.println(binarySearch(list, 1));
+//        System.out.println(binarySearch(list, 80));
+//    }
+//}
