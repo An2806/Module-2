66750cd4e266fc0641ec9778ffb8655e9d988f1e
An2806 <taman1842@gmail.com>
2025-06-17 10:37:20 +0700

    bai thuc hanh

---
+++ absolute-number-calculator/src/AbsoluteNumberCalculator.java
@@ -0,0 +1,8 @@
+public class AbsoluteNumberCalculator {
+    public static int finAbsolute(int number) {
+//        return 0;
+        if(number < 0)
+            return -number;
+        return number;
+    }
+}
